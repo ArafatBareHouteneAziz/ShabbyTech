4d57c3f4e0d44ad9cc2e5718545ffb052789e283
ArafatBareHouteneAziz <arafatbareaziz@gmail.com>
2025-06-22 13:59:56 +0100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
                 </div>
                 <!-- linked to Digit'All Website on same ip but different port -->
                 <a href="{{ env('DIGITAL_ALL_URL') }}" target="_blank" class="text-white/80 hover:text-white transition">
-                    Digit'All
+                    Digit'All {{ env('DIGITAL_ALL_URL') }}
                 </a>
             </div>
             <button class="md:hidden text-white hover:text-white/80">
